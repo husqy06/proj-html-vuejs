5702c109f667a10952969975e58bab880409eb89
husqy06 <luigf306@gmail.com>
2021-10-01 12:47:11 +0200

    Completamento jumbo e prima sezione main

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <header>
-    <img src="../assets/img/construction_logo.png">
+    <img src="../assets/img/construction_logo.png" class="logo">
 
     <ul>
       <li v-for="(item, index) in navItem" :key="index">
-        <a :href="item.href">{{ item.text }}</a>
+        <a :href="item.href" :class="item.href == 'current' ? 'current-link' : ''">{{ item.text }}</a>
       </li>
       <li><span class="small-botton">get quote</span></li>
     </ul>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,50 @@
 <template>
   <main>
+    <section class="jumbotron">
+      <span class="jumbo-title">building inspiring spaces</span>
+      <span class="jumbo-subtitle">We Build Inspiring Residential & Commercial Spaces</span>
+      <div>
+        <span class="big-botton" id="botton-black">exolore recent work</span>
+        <span class="big-botton">get a quote today</span>
+      </div>
+    </section>
 
+    <section class="quote-section">
+      <span class="section-title">Do You Have A Construction Project We Can Help With?</span>
+      <span class="small-botton quote-botton" id="botton-black">get a free quote</span>
+    </section>
+
+    <section class="main-section">
+      <span class="section-title">Specialist In Modern Construction</span>
+      <div class="section-line"></div>
+      <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi aperiam dolorem maxime blanditiis voluptatum. Similique fuga vitae culpa eum odio minima?</p>
+
+      <div class="section-card-container">
+        <div class="section-card custom-card-1">
+          <span class="custom-icon-1"><i class="far fa-building"></i></span>
+          <span class="section-title">Buildings</span>
+          <p class="section-paragraph">Quis ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam blandaditiis.</p>
+        </div>
+
+        <div class="section-card custom-card-1">
+          <span class="custom-icon-1"><i class="fas fa-sync"></i></span>
+          <span class="section-title">Renovate</span>
+          <p class="section-paragraph">Quis ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam blandaditiis.</p>
+        </div>
+
+        <div class="section-card custom-card-1">
+          <span class="custom-icon-1"><i class="fas fa-home"></i></span>
+          <span class="section-title">Construct</span>
+          <p class="section-paragraph">Quis ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam blandaditiis.</p>
+        </div>
+
+        <div class="section-card custom-card-1">
+          <span class="custom-icon-1"><i class="fas fa-truck"></i></span>
+          <span class="section-title">Exclusive</span>
+          <p class="section-paragraph">Quis ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam blandaditiis.</p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
