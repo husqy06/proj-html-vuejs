44376b32f67fe063f1154102c7214d55ced8e074
husqy06 <luigf306@gmail.com>
2021-10-04 19:19:25 +0200

    Completamento footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,94 @@
 <template>
   <footer>
+    <div class="footer-divider"></div>
 
+    <section class="footer-top">
+
+      <div class="footer-card">
+        <img src="../assets/img/footer-logo.png" class="logo">
+        <p>We are specialist in the construction of unique and exlusive properties. Our work inspires. We pride ourselves on delivering outstanding quality and design for leading clients across the world.</p>
+        <span class="small-botton">learn more</span>
+      </div>
+
+      <div class="footer-card">
+        <span class="footer-section-title">latest tweets</span>
+        <div class="contact-info">
+          <img src="../assets/img/3a74ce3d0532b7773b174c45ca3bd05a_bigger.png" alt="" class="twitter-img">
+
+          <div>
+            <div class="custom-contact-info">
+              <div>
+                <span class="custom-subtitle">ThemeFusion</span>
+                <span id="twitter-account">@Theme_Fusion</span>
+              </div>
+              <i class="fab fa-twitter"></i>
+            </div>
+
+            <p class="custom-paragraph-2">Do you need sublime WordPress hosting for your next website? Take advantage of exlusive partner offers that we have secured just for you, & launch your site in seconds with <a href="">#avada</a> on WP Engine hosting & get 30% Off this Black Friday <a href="">bit.ly/3kjLLE2 #BlackFriday2020</a></p>
+
+            <img src="../assets/img/En2TRxLW4AEiWUN.jpeg" alt="" srcset="" class="coupon">
+
+            <div class="coupon-icon">
+              <div>
+                <i class="far fa-heart"></i>
+                <i class="fas fa-sign-out-alt"></i>
+              </div>
+              
+              <span>22h</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-card">
+        <span class="footer-section-title">contact us today</span>
+
+        <div class="contact-info">
+          <i class="fas fa-globe"></i>
+          <span class="footer-text">Corporate Location <br> 1600 Amphitheatre Parkway <br> London WC1 1BA</span>
+        </div>
+
+        <div class="contact-info"> 
+          <i class="fas fa-home"></i>
+          <span class="footer-text">Residential Location <br> 9521 BroadsBerry Avenue <br> Paddington RC7 9ZA</span>
+        </div>
+
+        <div class="contact-info">
+          <i class="fas fa-phone"></i>
+          <span class="footer-text">1.800.458.556 / 1.800.532.2112 </span>
+        </div>
+
+        <div class="contact-info">
+          <i class="far fa-envelope"></i>
+          <span class="footer-text">info@your-domain.com</span>
+        </div>
+        
+        <div class="contact-info">
+          <i class="far fa-clock"></i>
+          <span class="footer-text">Monday - Friday: 9:00 AM - 6:00 PM</span>
+        </div>
+
+        <div class="contact-info">
+          <i class="far fa-clock"></i>
+          <span class="footer-text">Saturday - Sunday: 9:00 AM - 12:00 PM</span>
+        </div>
+      </div>
+
+      <div class="footer-card">
+        <span class="footer-section-title">find us</span>
+        <img src="../assets/img/map.jpg" class="map">
+      </div>
+    </section>
+    <section class="footer-bottom">
+      <span>COPYRIGHT 2012 - 2015 AVADA THEME BY THEME FUSION | ALL RIGHTS RESERVED | POWERED BY WORDPRESS</span>
+
+      <div>
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-youtube"></i>
+        <i class="fab fa-instagram"></i>
+      </div>
+    </section>
   </footer>
 </template>
 
