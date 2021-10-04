8e9e6b5fa882357eeec8fc937a5ee1a071416a6d
husqy06 <luigf306@gmail.com>
2021-10-04 16:06:49 +0200

    Completamento main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -45,6 +45,170 @@
         </div>
       </div>
     </section>
+
+    <section class="main-section custom-section-1">
+      <div class="section-card-container">
+        <div class="section-card" id="custom-card-2">
+          <i class="fas fa-suitcase"></i>
+          <span class="custom-title-1">3534</span>
+          <span class="section-view" id="custom-view-1">planning application</span>
+        </div>
+
+        <div class="section-card" id="custom-card-2">
+          <i class="far fa-building"></i>
+          <span class="custom-title-1">8<span class="custom-number-m">9</span>6</span>
+          <span class="section-view" id="custom-view-1">completed projects</span>
+        </div>
+
+        <div class="section-card" id="custom-card-2">
+          <i class="fas fa-users"></i>
+          <span class="custom-title-1">1<span class="custom-number-s">7</span>2</span>
+          <span class="section-view" id="custom-view-1">trained professionals</span>
+        </div>
+        
+        <div class="section-card" id="custom-card-2">
+          <i class="fas fa-globe"></i>
+          <span class="custom-title-1">1<span class="custom-number-s">9</span></span>
+          <span class="section-view" id="custom-view-1">international office</span>
+        </div>
+      </div>
+    </section>
+
+    <section class="main-section" id="custom-padding-1">
+      <span class="section-title">Explore Recent Work</span>
+      <div class="section-line"></div>
+      <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi aperiam dolorem maxime blanditiis voluptatum. Similique fuga vitae culpa eum odio minima?</p>
+
+      <div class="section-card-container">
+        <div class="section-card">
+          <img src="../assets/img/project2-featured-15013609.jpg" alt="" srcset="">
+        </div>
+
+        <div class="section-card">
+          <img src="../assets/img/project1-featured-294276386.jpg" alt="" srcset="">
+        </div>
+
+        <div class="section-card">
+          <img src="../assets/img/project3-featured-189023420.jpg">
+        </div>
+      </div>
+
+      <div class="section-view">
+        <span class="view-divider"></span>
+        <span>view all projects</span>
+        <span class="view-divider"></span>
+      </div>
+    </section>
+
+    <section class="main-section" id="custom-section-2">
+      <span class="section-title">Our Core Values</span>
+      <div class="section-line"></div>
+      <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi aperiam dolorem maxime blanditiis voluptatum. Similique fuga vitae culpa eum odio minima?</p>
+
+      <div class="section-card-container">
+        <div class="section-card" id="custom-card-3"> 
+          <span class="custom-icon-2 back-b"><i class="fas fa-home"></i></span>
+          <span class="custom-title-2">Great Services</span>
+          <span class="small-divider"></span>
+          <p class="section-paragraph">Quis ipsum dolor sit  aperiam blandaditiis consect adipisicing. Commodi aperiam blandaditiis.</p>
+        </div>
+        <div class="section-card" id="custom-card-3">
+          <span class="custom-icon-2 back-v"><i class="far fa-sun"></i></span>
+          <span class="custom-title-2">Highest Standards</span>
+          <span class="small-divider"></span>
+          <p class="section-paragraph">Quis ipsum dolor sit  aperiam blandaditiis consect adipisicing. Commodi aperiam blandaditiis.</p>
+        </div>
+        <div class="section-card" id="custom-card-3">
+          <span class="custom-icon-2 back-o"><i class="fas fa-users"></i></span>
+          <span class="custom-title-2">Professional Team</span>
+          <span class="small-divider"></span>
+          <p class="section-paragraph">Quis ipsum dolor sit  aperiam blandaditiis consect adipisicing. Commodi aperiam blandaditiis.</p>
+        </div>
+        <div class="section-card" id="custom-card-3">
+          <span class="custom-icon-2 back-g"><i class="far fa-lightbulb"></i></span>
+          <span class="custom-title-2">Creative Solutions</span>
+          <span class="small-divider"></span>
+          <p class="section-paragraph">Quis ipsum dolor sit  aperiam blandaditiis consect adipisicing. Commodi aperiam blandaditiis.</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="main-section custom-section-3">
+      <span class="section-title">Our Home Owners Say</span>
+      <div class="section-line"></div>
+      <img src="../assets/img/home-testimonial-113165296.jpg" alt="">
+      <p class="custom-paragraph-1">"No man but feels more of a man in the world if he have but a bit of ground that he can call his own. However small it is on the surface, it is four thousand miles deep; and that is a very handsome property."</p>
+      <span class="author">harry smith • new home owner</span>
+      <div><span class="dot-full"></span><span class="dot-empty"></span></div>
+    </section>
+
+    <section class="main-section" id="custom-section-5">
+      <span class="section-title">Latest News</span>
+      <div class="section-line"></div>
+      <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi aperiam dolorem maxime blanditiis voluptatum. Similique fuga vitae culpa eum odio minima?</p>
+
+      <div class="section-card-container">
+        <div class="section-card">
+          <img src="../assets/img/blog-post-134132600.jpg" alt="">
+          <div class="card-description">
+            <span class="custom-title-3">Redeveloping Florida's Remote Southern Coast</span>
+            <span class="date-section-5">December 7th, 2015</span>
+            <p class="section-paragraph">Technology is Here to Stay Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus purus nisl, elementum vitae consequat at, tristique ut enim, Sed ut dignissim leo. Nullam sed metus id sapien faucibus rhoncus sed</p>
+          </div>
+        </div>
+
+        <div class="section-card">
+          <img src="../assets/img/blog-post-92486644.jpg" alt="">
+          <div class="card-description">
+            <span class="custom-title-3">How We Manage Large Construction Projects</span>
+            <span class="date-section-5">December 7th, 2015</span>
+            <p class="section-paragraph">Technology is Here to Stay Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus purus nisl, elementum vitae consequat at, tristique ut enim, Sed ut dignissim leo. Nullam sed metus id sapien faucibus rhoncus sed</p>
+          </div>
+        </div>
+        <div class="section-card">
+          <img src="../assets/img/blog-post-332773904.jpg" alt="">
+          <div class="card-description">
+            <span class="custom-title-3">Future proofing a modern home</span>
+            <span class="date-section-5">December 6th, 2015</span>
+            <p class="section-paragraph">Technology is Here to Stay Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus purus nisl, elementum vitae consequat at, tristique ut enim, Sed ut dignissim leo. Nullam sed metus id sapien faucibus rhoncus sed</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="section-view">
+        <span class="view-divider"></span>
+        <span>view all articles</span>
+        <span class="view-divider"></span>
+      </div>
+    </section>
+
+    <section class="main-section custom-section-4">
+      <span class="section-title">Trusted Partners</span>
+      <div class="section-line"></div>
+      <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi aperiam dolorem maxime blanditiis voluptatum. Similique fuga vitae culpa eum odio minima?</p>
+
+      <div class="section-card-container">
+        <img src="../assets/img/home-logo11-219096700-320x202.png" alt="" srcset="">
+        <img src="../assets/img/home-logo10-219096700-320x202.png" alt="" srcset="">
+        <img src="../assets/img/home-logo2-219096700-320x202.png">
+        <img src="../assets/img/home-logo1-219096700-320x202.png">
+        <img src="../assets/img/home-logo9-219096700-320x202.png" alt="">
+      </div>
+    </section>
+
+    <section class="custom-section-6">
+      <div class="section-card-container">
+        <div class="custom-card-4">
+          <span class="section-title">Building Inspiring Spaces</span>
+          <div class="section-line"></div>
+          <p>Vivamus sed consectetur tellus dolor sit amet, consectetur adipiscing lorem edit sadip ipsum ets ficilis uns.</p>
+        </div>
+        <div class="section-card">
+          <span class="big-botton">get a quote today</span>
+          <span class="custom-subtitle">Only takes a few seconds!</span>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
